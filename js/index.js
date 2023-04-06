229b4e3222224c069782bb435388bd5b526f689d
Johnson <mikesonowallah@gmail.com>
2023-04-06 10:10:41 +0300

    Made an attempt to optimize my js code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 //My code goes here
-const myApi = 'https://api.artic.edu/api/v1/artworks?page=2&limit=50'
-let art = []
+
+const myApi = 'https://api.artic.edu/api/v1/artworks?page=2&limit=50';
+const artList = document.querySelector('.art-list');
+const searchedArtList = document.querySelector('#searched-art-list');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
 document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault()
     fetchArtItems()
@@ -38,7 +43,6 @@ const fetchArtItems = async () => {
  */
 
 const renderArtItems = function (artItems) {
-    let artList = document.querySelector('.art-list')
     artItems.forEach(artItem => {
         console.log(artItem.api_link);
         //https://api.artic.edu/api/v1/artworks/27992?fields=id,title,image_id
@@ -66,56 +70,158 @@ const renderArtItems = function (artItems) {
 /**
  * the following function fetches a searched item
  */
-const searchArtItem = function (searchValue) {
+
+const searchArtItem = async function (searchValue) {
     let searchPath = `https://api.artic.edu/api/v1/artworks/search?q=${searchValue}&&fields=id`
-    return fetch(searchPath)
-    .then(response => response.json())
-    .then(res => {
-        console.log(res.data);
-        fetchSearchedItems(res.data)
-    })
-    .catch(error => console.error(error.message))
+    try {
+        const response = await fetch(searchPath)
+        const res = await response.json()
+        return await fetchSearchedItems(res.data)
+    } catch (error) {
+        return console.error(error.message)
+    }
 }
 
-/**
- * the following function fetches the searched items
- */
-const fetchSearchedItems = function (response) {
-    response.forEach(element => {
+const fetchSearchedItems = async function (response) {
+    const artworkPromises = response.map(async element => {
         let apiPath = `https://api.artic.edu/api/v1/artworks/${element.id}`
-        return fetch(apiPath)
-        .then(res => res.json())
-        .then(jsonData => {
-            // console.log(data);
-            renderSearchedArtItems(jsonData.data)
-        })
+        const res = await fetch(apiPath);
+        const jsonData = await res.json();
+        return jsonData.data;
     });
-    
-    
-    
-}
 
-/**
- * the following functionrenders the seached items
- */
+    const searchedItems = await Promise.all(artworkPromises)
+    return renderSearchedArtItems(searchedItems)
+}
 
 function renderSearchedArtItems(searchedItems) {
-
-    let searchedArtList = document.querySelector('#searched-art-list')
-        console.log(searchedItems.title);
-        
-        let imageSrc = `https://www.artic.edu/iiif/2/${searchedItems.image_id}/full/843,/0/default.jpg`
+    searchedItems.forEach(searchedItem => {
+        let imageSrc = `https://www.artic.edu/iiif/2/${searchedItem.image_id}/full/843,/0/default.jpg`
 
         let artListItem = document.createElement('li')
         artListItem.className = `artListItem`
         artListItem.innerHTML = `
         <div class="art-item shadow">
-            <img src='${imageSrc}' alt="${searchedItems.thumbnail.alt_text}" id="art-item-image">
-            <h3 id="art-item-title">${searchedItems.title}</h3>
-            <h4 id="art-item-artist-display">by: <span id='artist-name'>${searchedItems.artist_display}</span></h4>
+            <img src='${imageSrc}' alt="${searchedItem.thumbnail.alt_text}" id="art-item-image">
+            <h3 id="art-item-title">${searchedItem.title}</h3>
+            <h4 id="art-item-artist-display">by: <span id='artist-name'>${searchedItem.artist_display}</span></h4>
         </div>
         `
         searchedArtList.appendChild(artListItem)
+    });
+};
+
+
+// const searchArtItem = function (searchValue) {
+//     let searchPath = `https://api.artic.edu/api/v1/artworks/search?q=${searchValue}&&fields=id`
+//     return fetch(searchPath)
+//     .then(response => response.json())
+//     .then(res => {
+//         console.log(res.data);
+//         fetchSearchedItems(res.data)
+//     })
+//     .catch(error => console.error(error.message))
+// }
+
+// /**
+//  * the following function fetches the searched items
+//  */
+// const fetchSearchedItems = function (response) {
+//     response.forEach(element => {
+//         let apiPath = `https://api.artic.edu/api/v1/artworks/${element.id}`
+//         return fetch(apiPath)
+//         .then(res => res.json())
+//         .then(jsonData => {
+//             // console.log(data);
+//             renderSearchedArtItems(jsonData.data)
+//         })
+//     });
+    
+    
+    
+// }
+
+// /**
+//  * the following functionrenders the seached items
+//  */
+
+// function renderSearchedArtItems(searchedItems) {
+
+//     let searchedArtList = document.querySelector('#searched-art-list')
+//         console.log(searchedItems.title);
         
-    };
+//         let imageSrc = `https://www.artic.edu/iiif/2/${searchedItems.image_id}/full/843,/0/default.jpg`
+
+//         let artListItem = document.createElement('li')
+//         artListItem.className = `artListItem`
+//         artListItem.innerHTML = `
+//         <div class="art-item shadow">
+//             <img src='${imageSrc}' alt="${searchedItems.thumbnail.alt_text}" id="art-item-image">
+//             <h3 id="art-item-title">${searchedItems.title}</h3>
+//             <h4 id="art-item-artist-display">by: <span id='artist-name'>${searchedItems.artist_display}</span></h4>
+//         </div>
+//         `
+//         searchedArtList.appendChild(artListItem)
+        
+//     };
+
+
+
+// const myApi = 'https://api.artic.edu/api/v1/artworks?page=2&limit=50';
+// const artList = document.querySelector('.art-list');
+// const searchedArtList = document.querySelector('#searched-art-list');
+// const form = document.querySelector('form');
+// const input = document.querySelector('input');
+
+// const fetchArtItems = async () => {
+//   try {
+//     const response = await fetch(myApi, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+//       },
+//     });
+//     const { data: artItems } = await response.json();
+//     renderArtItems(artItems);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
+
+// const renderArtItems = (artItems) => {
+//   const artElements = artItems.map((artItem) => {
+//     const { title, image_id, thumbnail, artist_display } = artItem;
+//     const imageSrc = `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`;
+//     const artListItem = document.createElement('li');
+//     artListItem.className = `artListItem`;
+//     artListItem.innerHTML = `
+//         <div class="art-item shadow">
+//             <img src='${imageSrc}' alt="${thumbnail.alt_text}" id="art-item-image">
+//             <h3 id="art-item-title">${title}</h3>
+//             <h4 id="art-item-artist-display">by: <span id='artist-name'>${artist_display}</span></h4>
+//             <div class='buttons'>
+//             <button id='like'>like</button>
+//             <button id='share'>share</button>
+//             <button id='download'>download</button>
+//             </div>
+//         </div>
+//         `;
+//     return { title, artist: artist_display, element: artListItem };
+//   });
+//   artList.append(...artElements.map((element) => element.element));
+// };
+
+// const searchArtItem = async (searchValue) => {
+//   const searchPath = `https://api.artic.edu/api/v1/artworks/search?q=${searchValue}&&fields=id`;
+//   try {
+//     const response = await fetch(searchPath);
+//     const { data: searchedItems } = await response.json();
+//     const promises = searchedItems.map(async (element) => {
+//       const apiPath = `https://api.artic.edu/api/v1/artworks/${element.id}`
+//     }
+//     )}
+//     catch(error){
+//         console.error(error.message);
+//     }
+// }
 
